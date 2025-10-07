2476a24e748ba28b8dc69bfb37f8c9cec2fd6f25
nsymk <k025g1028@g.neec.ac.jp>
2025-10-07 10:14:59 +0900

    プレイヤーのジャンプ実装

--- PM1_Git_1.cpp
+++ PM1_Git_1.cpp
@@ -6,6 +6,7 @@
 int main()
 {
     std::cout << "Hello World!\n";
+    std::cout << "プレイヤーがジャンプできるようになった\n";
 }
 
 // プログラムの実行: Ctrl + F5 または [デバッグ] > [デバッグなしで開始] メニュー
@@ -22,3 +23,4 @@ int main()
 
 
 //aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+
