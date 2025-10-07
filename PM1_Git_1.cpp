381e23cdafc5f52ffbbfd6c3a2c4d3d4fc8b7961
nsymk <k025g1028@g.neec.ac.jp>
2025-10-07 10:41:59 +0900

    10の速度でplayerを移動

--- PM1_Git_1.cpp
+++ PM1_Git_1.cpp
@@ -5,9 +5,13 @@
 
 int main()
 {
+
+    int playerSpeed = 10;
+
     std::cout << "Hello World!\n";
     std::cout << "プレイヤーがジャンプできるようになった\n";
     std::cout << "プレイヤーが移動できるようになった\n";
+    std::cout << "プレイヤーがplayerSpeedの速さで移動\n";
 }
 
 // プログラムの実行: Ctrl + F5 または [デバッグ] > [デバッグなしで開始] メニュー
